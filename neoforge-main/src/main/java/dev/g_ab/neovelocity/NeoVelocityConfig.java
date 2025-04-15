55bfc2a12d18c5b7398d7fc70b3ea1a27f10153c
Gabwasnt <113859353+Gabwasnt@users.noreply.github.com>
2025-04-15 16:29:04 -0400

    Rough Daft
    -Secret is now stored in bytes inside the config class.
    -If the `forwarding-secret` string ends with `.secret`, it considers it a file.

--- neoforge-main/src/main/java/dev/g_ab/neovelocity/NeoVelocityConfig.java
+++ neoforge-main/src/main/java/dev/g_ab/neovelocity/NeoVelocityConfig.java
@@ -4,8 +4,15 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class NeoVelocityConfig {
     public static final NeoVelocityConfig.Common COMMON;
@@ -20,6 +27,16 @@ public class NeoVelocityConfig {
 
     public static void onFileChange(final ModConfigEvent.Reloading configEvent) {
         NeoVelocity.getLogger().debug("Detected new secret!");
+        if (COMMON.SECRET.get().endsWith(".secret")) {
+            Path path = FMLPaths.CONFIGDIR.get().resolve(COMMON.SECRET.get());
+            if (!path.toFile().exists()) {
+                NeoVelocity.getLogger().warn("The secret file {}, is not present!", path);
+            } else try {
+                COMMON.secret = Files.readAllBytes(path);
+            } catch (IOException e) {
+                COMMON.secret = null;
+            }
+        } else COMMON.secret = COMMON.SECRET.get().getBytes(StandardCharsets.UTF_8);
     }
 
     public static void register(IEventBus bus, ModContainer mod) {
@@ -28,12 +45,13 @@ public class NeoVelocityConfig {
     }
 
     public static class Common {
-        public ModConfigSpec.ConfigValue<String> SECRET;
+        private ModConfigSpec.ConfigValue<String> SECRET;
+        public byte @Nullable [] secret;
 
         Common(ModConfigSpec.Builder builder) {
             SECRET = builder
-                .comment("The Forwarding secret of your velocity proxy")
-                .define("forwarding-secret", "secret!");
+                    .comment("The Forwarding secret of your velocity proxy, if the provided string ends with `.secret`, it considers it a file.")
+                    .define("forwarding-secret", "forwarding.secret");
         }
     }
 }
