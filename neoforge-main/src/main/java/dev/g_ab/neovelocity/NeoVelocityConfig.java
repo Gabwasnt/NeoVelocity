859e6e8e246b94897e451998f133bcc666096eb0
G_ab <contact@g-ab.dev>
2025-04-16 19:16:23 -0400

    Fixes + Config comments

--- neoforge-main/src/main/java/dev/g_ab/neovelocity/NeoVelocityConfig.java
+++ neoforge-main/src/main/java/dev/g_ab/neovelocity/NeoVelocityConfig.java
@@ -18,7 +18,6 @@ public class NeoVelocityConfig {
     public static final NeoVelocityConfig.Common COMMON;
     static final ModConfigSpec commonSpec;
 
-
     static {
         final Pair<NeoVelocityConfig.Common, ModConfigSpec> specPair = new ModConfigSpec.Builder().configure(NeoVelocityConfig.Common::new);
         commonSpec = specPair.getRight();
@@ -26,21 +25,16 @@ public class NeoVelocityConfig {
     }
 
     public static void onFileChange(final ModConfigEvent.Reloading configEvent) {
-        NeoVelocity.getLogger().debug("Detected new secret!");
-        if (COMMON.SECRET.get().endsWith(".secret")) {
-            Path path = FMLPaths.CONFIGDIR.get().resolve(COMMON.SECRET.get());
-            if (!path.toFile().exists()) {
-                NeoVelocity.getLogger().warn("The secret file {}, is not present!", path);
-            } else try {
-                COMMON.secret = Files.readAllBytes(path);
-            } catch (IOException e) {
-                COMMON.secret = null;
-            }
-        } else COMMON.secret = COMMON.SECRET.get().getBytes(StandardCharsets.UTF_8);
+        COMMON.updateSecretFromConfig();
+    }
+
+    public static void onFileLoad(final ModConfigEvent.Loading configEvent) {
+        COMMON.updateSecretFromConfig();
     }
 
     public static void register(IEventBus bus, ModContainer mod) {
         bus.addListener(NeoVelocityConfig::onFileChange);
+        bus.addListener(NeoVelocityConfig::onFileLoad);
         mod.registerConfig(ModConfig.Type.COMMON, NeoVelocityConfig.commonSpec);
     }
 
@@ -50,8 +44,23 @@ public class NeoVelocityConfig {
 
         Common(ModConfigSpec.Builder builder) {
             SECRET = builder
-                    .comment("The Forwarding secret of your velocity proxy, if the provided string ends with `.secret`, it considers it a file.")
-                    .define("forwarding-secret", "forwarding.secret");
+                    .comment("The Forwarding secret of your velocity proxy.")
+                    .comment("If the provided string ends with `.secret`, it is treated as a path to a file specified in the server's config directory.")
+                    .comment("The file is expected to be UTF-8 encoded and not empty.")
+                    .define("forwarding-secret", "Insert-Here");
+        }
+
+        private void updateSecretFromConfig(){
+            if (this.SECRET.get().endsWith(".secret")) {
+                Path path = FMLPaths.CONFIGDIR.get().resolve(this.SECRET.get());
+                if (!path.toFile().exists()) {
+                    NeoVelocity.getLogger().warn("The secret file at {} , is not present!", path);
+                } else try {
+                    this.secret = Files.readString(path).getBytes(StandardCharsets.UTF_8);
+                } catch (IOException e) {
+                    this.secret = null;
+                }
+            } else this.secret = this.SECRET.get().getBytes(StandardCharsets.UTF_8);
         }
     }
 }
