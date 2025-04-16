c4c6be3ee26e914d6691388344830edcdcef1ece
G_ab <contact@g-ab.dev>
2025-04-16 19:56:53 -0400

    Path config dir -> running dir

--- neoforge-main/src/main/java/dev/g_ab/neovelocity/NeoVelocityConfig.java
+++ neoforge-main/src/main/java/dev/g_ab/neovelocity/NeoVelocityConfig.java
@@ -4,7 +4,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.config.ModConfigEvent;
-import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.Nullable;
@@ -12,6 +11,7 @@ import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 
 public class NeoVelocityConfig {
@@ -44,20 +44,24 @@ public class NeoVelocityConfig {
 
         Common(ModConfigSpec.Builder builder) {
             SECRET = builder
-                    .comment("The Forwarding secret of your velocity proxy.")
-                    .comment("If the provided string ends with `.secret`, it is treated as a path to a file specified in the server's config directory.")
-                    .comment("The file is expected to be UTF-8 encoded and not empty.")
-                    .define("forwarding-secret", "Insert-Here");
+                .comment("The Forwarding secret of your velocity proxy.")
+                .comment("If the provided string ends with `.secret`, it is treated as a path to a file in the running directory.")
+                .comment("The file is expected to be UTF-8 encoded and not empty.")
+                .define("forwarding-secret", "Insert-Here");
         }
 
-        private void updateSecretFromConfig(){
+        private void updateSecretFromConfig() {
             if (this.SECRET.get().endsWith(".secret")) {
-                Path path = FMLPaths.CONFIGDIR.get().resolve(this.SECRET.get());
-                if (!Files.exists(path)) {
-                    NeoVelocity.getLogger().warn("The secret file at {} , is not present!", path);
-                } else try {
-                    this.secret = String.join("", Files.readAllLines(path)).getBytes(StandardCharsets.UTF_8);
-                } catch (IOException e) {
+                try {
+                    Path path = Path.of(this.SECRET.get());
+                    if (!Files.exists(path)) {
+                        NeoVelocity.getLogger().warn("The secret file at {} , is not present!", path);
+                    } else try {
+                        this.secret = String.join("", Files.readAllLines(path)).getBytes(StandardCharsets.UTF_8);
+                    } catch (IOException e) {
+                        this.secret = null;
+                    }
+                } catch (InvalidPathException e) {
                     this.secret = null;
                 }
             } else this.secret = this.SECRET.get().getBytes(StandardCharsets.UTF_8);
